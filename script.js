c14985809157b3ae0d6928dbee191fef28680072
Alfredo <alfrdons.jr@gmail.com>
2026-05-06 15:00:28 -0300

    Update button text based on theme toggle in script.js

--- script.js
+++ script.js
@@ -3,4 +3,12 @@
 const switcher = document.querySelector('.btn');
 switcher.addEventListener('click', function() {
     document.body.classList.toggle('dark-theme')
+
+    var className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Dark Theme";
+    }
+    else {
+        this.textContent = "Light Theme";
+    }
 });
